bc173dd2289c9adf9db9e758b0d14f4f61696bfd
aryobarzanirani <aryobarzan.iranii@googlemail.com>
2026-07-02 00:49:15 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,35 +1,14 @@
 export default {
-  async fetch(request, env) {
+  async fetch(request) {
 
-    if (request.method !== "POST") {
-      return new Response("DEBUG Worker");
+    if (request.method === "GET") {
+      return new Response("GET OK");
     }
 
-    const update = await request.json();
-
-    if (!update.message || !update.message.text) {
-      return new Response("OK");
+    if (request.method === "POST") {
+      return new Response("POST OK");
     }
 
-    const chatId = update.message.chat.id;
-
-    let text = "";
-
-    text += env.TELEGRAM_BOT_TOKEN ? "✅ TELEGRAM_SECRET\n" : "❌ TELEGRAM_SECRET\n";
-    text += env.GEMINI_API_KEY ? "✅ GEMINI_SECRET\n" : "❌ GEMINI_SECRET\n";
-
-    await fetch(
-      `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          chat_id: chatId,
-          text: text
-        })
-      }
-    );
-
-    return new Response("OK");
+    return new Response("OTHER");
   }
 }

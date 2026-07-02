22d6e029d29a9fa226166bba3fddf8deda462d72
aryobarzanirani <aryobarzan.iranii@googlemail.com>
2026-07-02 17:23:34 +0330

    Update telegram.js

--- src/telegram.js
+++ src/telegram.js
@@ -1,17 +1,24 @@
 export async function sendTelegram(env, chatId, text) {
+  try {
+    const response = await fetch(
+      `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          chat_id: chatId,
+          text: text
+        })
+      }
+    );
 
-  await fetch(
-    `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        chat_id: chatId,
-        text
-      })
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error("Telegram Error:", errorText);
     }
-  );
-
+  } catch (error) {
+    console.error("Telegram Fetch Error:", error);
+  }
 }

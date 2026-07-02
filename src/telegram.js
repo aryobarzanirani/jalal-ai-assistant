9dac9a2a44db570fcfc285c361a17741a259c55d
aryobarzanirani <aryobarzan.iranii@googlemail.com>
2026-07-02 16:03:29 +0330

    Create telegram.js

---
+++ src/telegram.js
@@ -0,0 +1,17 @@
+export async function sendTelegram(env, chatId, text) {
+
+  await fetch(
+    `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text
+      })
+    }
+  );
+
+}

fe5b83ac7e53d027a4034e3db85626a106fd3337
aryobarzanirani <aryobarzan.iranii@googlemail.com>
2026-07-06 13:50:50 +0330

    Update search-utils.js

--- src/search-utils.js
+++ src/search-utils.js
@@ -1,3 +1,5 @@
+import { expandWords }
+from "./synonyms.js";
 import { expandWords } from "./synonyms.js";
 function normalize(text) {
 
@@ -8,6 +10,31 @@ function normalize(text) {
     .replace(/[^\p{L}\p{N}\s]/gu," ")
     .replace(/\s+/g," ")
     .trim();
+
+}
+
+function expandDynamic(memory, words) {
+
+  const result = new Set(words);
+
+  if (!memory?.dynamicSynonyms) {
+    return [...result];
+  }
+
+  for (const word of words) {
+
+    const list =
+      memory.dynamicSynonyms[word];
+
+    if (list) {
+
+      list.forEach(x => result.add(x));
+
+    }
+
+  }
+
+  return [...result];
 
 }
 
